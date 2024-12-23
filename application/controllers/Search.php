34f59b3ec79795a20d1ddb6fd9175f49ee41725b
galihnkk <galihnkk96@gmail.com>
2024-12-23 22:08:11 +0700

    up

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -4,10 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Search extends CI_Controller {
 
     public function index() {
+        $this->data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
         $query = $this->input->get('query');
         $this->load->model('Search_model');
-        $data['results'] = $this->Search_model->search($query);
-        $this->load->view('fronts/search_results', $data);
+        $this->data['results'] = $this->Search_model->search($query);
+        $this->load->view('fronts/search_results', $this->data);
     }
 }
 ?>
