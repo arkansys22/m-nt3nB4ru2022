48a034fa31652b89632ff6b18d26efb827010ec6
galihnkk <galihnkk96@gmail.com>
2024-12-24 12:11:52 +0700

    up

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -6,6 +6,7 @@ class Search_model extends CI_Model {
             return [];
         }
         $this->db->like('judul', $query);
+        $this->db->order_by('RAND()'); // Add this line to randomize results
         $results = $this->db->get('harga');
         return $results->result();
     }
