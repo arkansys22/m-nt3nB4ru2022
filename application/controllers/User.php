9a172c861810b269feaa280a25b90857b660213d
galihnkk <galihnkk96@gmail.com>
2025-01-30 13:54:46 +0700

    feat(data) : submit in data pengantin now connected with table pengantin_islam and pengantin_lainnya

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -142,13 +142,13 @@ class User extends CI_Controller
         $this->form_validation->set_rules('cpp_namaleng','','trim|required', array('trim' => '', 'required' => 'nama lengkap pengantin pria belum diisi'));
 
 
-        $this->form_validation->set_rules('email','','trim|required|valid_email|is_unique[user.email]', array('trim' => '', 'required' => 'Email belum diisi','is_unique' => 'Email telah digunakan, silahkan gunakan email lain.'));
-        $this->form_validation->set_rules('password','','trim|required', array('trim' => '', 'required' => 'Password belum diisi'));
-        $this->form_validation->set_rules('konfirmpassword','','trim|required|matches[password]', array('trim' => '', 'required' => 'Konfirmasi Password belum diisi','matches'=>'Password tidak sama! Cek kembali password Anda'));
+        // $this->form_validation->set_rules('email','','trim|required|valid_email|is_unique[user.email]', array('trim' => '', 'required' => 'Email belum diisi','is_unique' => 'Email telah digunakan, silahkan gunakan email lain.'));
+        // $this->form_validation->set_rules('password','','trim|required', array('trim' => '', 'required' => 'Password belum diisi'));
+        // $this->form_validation->set_rules('konfirmpassword','','trim|required|matches[password]', array('trim' => '', 'required' => 'Konfirmasi Password belum diisi','matches'=>'Password tidak sama! Cek kembali password Anda'));
        
-        if($this->form_validation->run() != false){
+        // if($this->form_validation->run() != false){
           if (isset($_POST['submit']))
-          {
+          {var_dump($_POST);
             $username = $this->input->post('username');
             $email = $this->input->post('email');            
             $password = sha1($this->input->post('password'));
@@ -157,7 +157,7 @@ class User extends CI_Controller
             if ($total > 0)
               {
               $data['title'] = 'Periksa kembali email dan password Anda!';
-              redirect(site_url('daftar'));
+              // redirect(site_url('daftar'));
               }else{
                 $saltid   = md5($email);
                 $aktivasi   = 0;
@@ -178,12 +178,12 @@ class User extends CI_Controller
                   if($this->sendemail($email,$saltid,$username))
                                 {
                                     $this->session->set_flashdata('msg','<div class="alert bg-5 text-center">Segera lakukan aktivasi akun mantenbaru dari email anda. Harap merefresh pesan masuk di email Anda.</div>');
-                                    redirect(base_url('daftar')
-                                    );
+                                    // redirect(base_url('daftar')
+                                    // );
                             }else
                                 {
                                   $this->session->set_flashdata('msg','<div class="alert bg-5 text-center">Email Verifikasi tidak terkirim</div>');
-                                  redirect(base_url('daftar'));
+                                  // redirect(base_url('daftar'));
                                 }
                 }
                 $data['title'] = 'Sukses mendaftar';
@@ -198,12 +198,13 @@ class User extends CI_Controller
               $this->load->view('fronts/user/v_datapengantin',$data);
             }
 
-          } else{
-          $data['title'] = 'Ops.. Masih ada yang kurang. Silahkan dicek kembali.';
-          $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
-          $this->load->view('fronts/user/v_datapengantin',$data);
-        }
-  }
+          } 
+  //         else{
+  //         $data['title'] = 'Ops.. Masih ada yang kurang. Silahkan dicek kembali.';
+  //         $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+  //         $this->load->view('fronts/user/v_datapengantin',$data);
+  //       }
+  // }
   public function sendemail($email,$saltid,$username){
         // configure the email setting
 
@@ -238,7 +239,74 @@ class User extends CI_Controller
           }
       }
 
+  public function submit_data_pengantin() {
+    $agama = $this->input->post('agama');
 
+    if ($agama == 'islam') {
+      $data = array(
+      'customers_islam_cpw_namaleng' => $this->input->post('customers_' . $agama . '_cpw_namaleng'),
+      'customers_islam_cpw_namapang' => $this->input->post('customers_' . $agama . '_cpw_namapang'),
+      'customers_islam_cpw_anakke' => $this->input->post('customers_' . $agama . '_cpw_anakke'),
+      'customers_islam_cpw_anakdari' => $this->input->post('customers_' . $agama . '_cpw_anakdari'),
+      'customers_islam_cpw_namabpk' => $this->input->post('customers_' . $agama . '_cpw_namabpk'),
+      'customers_islam_cpw_panggilkebpk' => $this->input->post('customers_' . $agama . '_cpw_panggilkebpk'),
+      'customers_islam_cpw_namaibu' => $this->input->post('customers_' . $agama . '_cpw_namaibu'),
+      'customers_islam_cpw_panggilkeibu' => $this->input->post('customers_' . $agama . '_cpw_panggilkeibu'),
+      'customers_islam_cpw_saudara' => $this->input->post('customers_' . $agama . '_cpw_saudara'),
+      'customers_islam_cpp_namaleng' => $this->input->post('customers_' . $agama . '_cpp_namaleng'),
+      'customers_islam_cpp_namapang' => $this->input->post('customers_' . $agama . '_cpp_namapang'),
+      'customers_islam_cpp_anakke' => $this->input->post('customers_' . $agama . '_cpp_anakke'),
+      'customers_islam_cpp_anakdari' => $this->input->post('customers_' . $agama . '_cpp_anakdari'),
+      'customers_islam_cpp_namabpk' => $this->input->post('customers_' . $agama . '_cpp_namabpk'),
+      'customers_islam_cpp_panggilkebpk' => $this->input->post('customers_' . $agama . '_cpp_panggilkebpk'),
+      'customers_islam_cpp_namaibu' => $this->input->post('customers_' . $agama . '_cpp_namaibu'),
+      'customers_islam_cpp_panggilkeibu' => $this->input->post('customers_' . $agama . '_cpp_panggilkeibu'),
+      'customers_islam_cpp_saudara' => $this->input->post('customers_' . $agama . '_cpp_saudara'),
+      'customers_islam_mahar' => $this->input->post('customers_' . $agama . '_mahar'),
+      'customers_islam_seserahan' => $this->input->post('customers_' . $agama . '_seserahan'),
+      'customers_islam_cpw_koor' => $this->input->post('customers_' . $agama . '_cpw_koor'),
+      'customers_islam_cpp_koor' => $this->input->post('customers_' . $agama . '_cpp_koor'),
+      'customers_islam_cpw_jubir' => $this->input->post('customers_' . $agama . '_cpw_jubir'),
+      'customers_islam_cpp_jubir' => $this->input->post('customers_' . $agama . '_cpp_jubir'),
+      'customers_islam_namapenghulu' => $this->input->post('customers_' . $agama . '_namapenghulu'),
+      'customers_islam_namawali' => $this->input->post('customers_' . $agama . '_namawali'),
+      'customers_islam_cpw_saksi' => $this->input->post('customers_' . $agama . '_cpw_saksi'),
+      'customers_islam_cpp_saksi' => $this->input->post('customers_' . $agama . '_cpp_saksi'),
+      'customers_islam_qori' => $this->input->post('customers_' . $agama . '_qori'),
+      'customers_islam_nasehat' => $this->input->post('customers_' . $agama . '_nasehat'),
+      'customers_islam_pengapit' => $this->input->post('customers_' . $agama . '_pengapit'),
+      'customers_islam_nampan' => $this->input->post('customers_' . $agama . '_nampan'),
+      'customers_islam_pemmas' => $this->input->post('customers_' . $agama . '_pemmas'),
+      'customers_islam_pemcin' => $this->input->post('customers_' . $agama . '_pemcin')
+      );
+      $this->db->insert('pengantin_islam', $data);
+    } else {
+      $data = array(
+        'customers_lain_cpw_namaleng' => $this->input->post('customers_' . $agama . '_cpw_namaleng'),
+        'customers_lain_cpw_namapang' => $this->input->post('customers_' . $agama . '_cpw_namapang'),
+        'customers_lain_cpw_anakke' => $this->input->post('customers_' . $agama . '_cpw_anakke'),
+        'customers_lain_cpw_anakdari' => $this->input->post('customers_' . $agama . '_cpw_anakdari'),
+        'customers_lain_cpw_namabpk' => $this->input->post('customers_' . $agama . '_cpw_namabpk'),
+        'customers_lain_cpw_panggilkebpk' => $this->input->post('customers_' . $agama . '_cpw_panggilkebpk'),
+        'customers_lain_cpw_namaibu' => $this->input->post('customers_' . $agama . '_cpw_namaibu'),
+        'customers_lain_cpw_panggilkeibu' => $this->input->post('customers_' . $agama . '_cpw_panggilkeibu'),
+        'customers_lain_cpw_saudara' => $this->input->post('customers_' . $agama . '_cpw_saudara'),
+        'customers_lain_cpp_namaleng' => $this->input->post('customers_' . $agama . '_cpp_namaleng'),
+        'customers_lain_cpp_namapang' => $this->input->post('customers_' . $agama . '_cpp_namapang'),
+        'customers_lain_cpp_anakke' => $this->input->post('customers_' . $agama . '_cpp_anakke'),
+        'customers_lain_cpp_anakdari' => $this->input->post('customers_' . $agama . '_cpp_anakdari'),
+        'customers_lain_cpp_namabpk' => $this->input->post('customers_' . $agama . '_cpp_namabpk'),
+        'customers_lain_cpp_panggilkebpk' => $this->input->post('customers_' . $agama . '_cpp_panggilkebpk'),
+        'customers_lain_cpp_namaibu' => $this->input->post('customers_' . $agama . '_cpp_namaibu'),
+        'customers_lain_cpp_panggilkeibu' => $this->input->post('customers_' . $agama . '_cpp_panggilkeibu'),
+        'customers_lain_cpp_saudara' => $this->input->post('customers_' . $agama . '_cpp_saudara'),
+        'customers_lain_namapendeta' => $this->input->post('customers_' . $agama . '_namapendeta'),
+        'customers_lain_gereja' => $this->input->post('customers_' . $agama . '_gereja'),
+        'customers_lain_doa' => $this->input->post('customers_' . $agama . '_doa')
+      );
+      $this->db->insert('pengantin_lainnya', $data);
+    }
 
-
+    redirect('user/success');
+  }
 }
